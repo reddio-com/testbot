c98d6a9af9df41fa937278f74a657e423206750b
Lawliet-Chan <1576710154@qq.com>
2024-01-17 15:31:33 +0800

    default cfg

--- cairoVM/conf.go
+++ cairoVM/conf.go
@@ -1,10 +1,32 @@
 package cairoVM
 
-import "github.com/NethermindEth/juno/utils"
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/NethermindEth/juno/utils"
+	"github.com/sirupsen/logrus"
+)
 
 type Config struct {
 	Network             utils.Network `toml:"network"`
 	TxVersion           string        `toml:"tx_version"`
 	AccountAddr         string        `toml:"account_addr"`
 	AccountCairoVersion int           `toml:"account_cairo_version"`
+}
+
+func LoadTomlConf(fpath string) *Config {
+	cfg := new(Config)
+	_, err := toml.DecodeFile(fpath, cfg)
+	if err != nil {
+		logrus.Panicf("load config-file(%s) error: %s ", fpath, err.Error())
+	}
+	return cfg
+}
+
+func DefaultCfg() *Config {
+	return &Config{
+		Network:             1,
+		TxVersion:           "0x1",
+		AccountAddr:         "",
+		AccountCairoVersion: 0,
+	}
 }
