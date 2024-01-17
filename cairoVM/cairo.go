5b4b77c102b695c5f498ef7bf62758cedc3aaaf6
Xinran <1576710154@qq.com>
2024-01-17 19:25:09 +0800

    account (#6)

--- cairoVM/cairo.go
+++ cairoVM/cairo.go
@@ -1,6 +1,7 @@
 package cairoVM
 
 import (
+	"context"
 	"time"
 
 	"github.com/NethermindEth/juno/core"
@@ -11,13 +12,14 @@ import (
 	"github.com/NethermindEth/juno/vm"
 )
 
-type CairoVM struct {
+type Cairo struct {
 	vm    vm.VM
 	state core.StateReader
+	acc   *Account
 	cfg   *Config
 }
 
-func NewCairoVM(cfg *Config) (*CairoVM, error) {
+func NewCairoVM(cfg *Config) (*Cairo, error) {
 	log, err := utils.NewZapLogger(utils.ERROR, true)
 	if err != nil {
 		return nil, err
@@ -30,23 +32,30 @@ func NewCairoVM(cfg *Config) (*CairoVM, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &CairoVM{
+	return &Cairo{
 		vm:    vm.New(log),
 		state: core.NewState(txn),
+		acc:   NewAccount(),
 		cfg:   cfg,
 	}, nil
 }
 
-func (c *CairoVM) HandleCall(call *rpc.FunctionCall, classHash *felt.Felt) ([]*felt.Felt, error) {
+func (c *Cairo) HandleCall(call *rpc.FunctionCall, classHash *felt.Felt) ([]*felt.Felt, error) {
 	return c.vm.Call(&call.ContractAddress, classHash, &call.EntryPointSelector, call.Calldata, 0, uint64(time.Now().Unix()), c.state, c.cfg.Network)
 }
 
-func (c *CairoVM) HandleDeployAccountTx(tx *core.DeployAccountTransaction) (*felt.Felt, error) {
+func (c *Cairo) HandleDeployAccountTx(tx *core.DeployAccountTransaction) (*felt.Felt, error) {
 	txnHash, err := core.TransactionHash(tx, c.cfg.Network)
 	if err != nil {
 		return nil, err
 	}
 	tx.TransactionHash = txnHash
+	sig, err := c.acc.Sign(context.Background(), txnHash)
+	if err != nil {
+		return nil, err
+	}
+	tx.TransactionSignature = sig
+
 	txs := []core.Transaction{tx}
 	_, traces, err := c.vm.Execute(txs, nil, 0, uint64(time.Now().Unix()), &felt.Zero, c.state, c.cfg.Network, nil, false, false, true, &felt.Zero, &felt.Zero, false)
 	if err != nil {
@@ -55,12 +64,18 @@ func (c *CairoVM) HandleDeployAccountTx(tx *core.DeployAccountTransaction) (*fel
 	return &traces[0].ConstructorInvocation.CallerAddress, nil
 }
 
-func (c *CairoVM) HandleDeclareTx(tx *core.DeclareTransaction) (*felt.Felt, error) {
+func (c *Cairo) HandleDeclareTx(tx *core.DeclareTransaction) (*felt.Felt, error) {
 	txnHash, err := core.TransactionHash(tx, c.cfg.Network)
 	if err != nil {
 		return nil, err
 	}
 	tx.TransactionHash = txnHash
+	sig, err := c.acc.Sign(context.Background(), txnHash)
+	if err != nil {
+		return nil, err
+	}
+	tx.TransactionSignature = sig
+
 	txs := []core.Transaction{tx}
 	_, traces, err := c.vm.Execute(txs, nil, 0, uint64(time.Now().Unix()), &felt.Zero, c.state, c.cfg.Network, nil, false, false, true, &felt.Zero, &felt.Zero, false)
 	if err != nil {
@@ -69,12 +84,18 @@ func (c *CairoVM) HandleDeclareTx(tx *core.DeclareTransaction) (*felt.Felt, erro
 	return traces[0].ExecuteInvocation.FunctionInvocation.ClassHash, nil
 }
 
-func (c *CairoVM) HandleInvokeTx(tx *core.InvokeTransaction) (*vm.TransactionTrace, error) {
+func (c *Cairo) HandleInvokeTx(tx *core.InvokeTransaction) (*vm.TransactionTrace, error) {
 	txnHash, err := core.TransactionHash(tx, c.cfg.Network)
 	if err != nil {
 		return nil, err
 	}
 	tx.TransactionHash = txnHash
+	sig, err := c.acc.Sign(context.Background(), txnHash)
+	if err != nil {
+		return nil, err
+	}
+	tx.TransactionSignature = sig
+
 	txs := []core.Transaction{tx}
 	_, traces, err := c.vm.Execute(txs, nil, 0, uint64(time.Now().Unix()), &felt.Zero, c.state, c.cfg.Network, nil, false, false, true, &felt.Zero, &felt.Zero, false)
 	if err != nil {
@@ -83,12 +104,14 @@ func (c *CairoVM) HandleInvokeTx(tx *core.InvokeTransaction) (*vm.TransactionTra
 	return &traces[0], nil
 }
 
-func (c *CairoVM) HandleL1HandlerTx(tx *core.L1HandlerTransaction) error {
+func (c *Cairo) HandleL1HandlerTx(tx *core.L1HandlerTransaction) error {
+	// L1 handle tx has no signature
 	txnHash, err := core.TransactionHash(tx, c.cfg.Network)
 	if err != nil {
 		return err
 	}
 	tx.TransactionHash = txnHash
+
 	txs := []core.Transaction{tx}
 	_, _, err = c.vm.Execute(txs, nil, 0, uint64(time.Now().Unix()), &felt.Zero, c.state, c.cfg.Network, []*felt.Felt{&felt.Zero}, false, false, true, &felt.Zero, &felt.Zero, false)
 	return err
