85c66f6db200daecb2aa8146fab1341e2b115d87
Qiang Zhou <zhouqiang.cl@gmail.com>
2024-02-01 15:44:54 +0800

    declare success
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- cairoVM/deploy.go
+++ cairoVM/deploy.go
@@ -106,7 +106,7 @@ func NewDeployCoolOld() (*core.InvokeTransaction, error) {
 	// using UniversalDeploy address
 	contractAddress := new(felt.Felt).SetUint64(1)
 
-	classHash, _ := new(felt.Felt).SetString("0x35eb1d3593b1fe9a8369a023ffa5d07d3b2050841cb75ad6ef00698d9307d10")
+	classHash, _ := new(felt.Felt).SetString("0x47f93257c3a6e42fc71162a646b3223dfad27c2d994f97f333492c66e31b8c8")
 
 	salt, _ := new(felt.Felt).SetString("0x53eb1d3593b1fe9a8369a023ffa5d07d3b2050841cb75ad6ef00698d9307d10")
 
@@ -128,13 +128,50 @@ func NewDeployCoolOld() (*core.InvokeTransaction, error) {
 
 	fmt.Println("invoke calldata = ", txCallData)
 
-	nonce := new(felt.Felt).SetUint64(2)
+	nonce := new(felt.Felt).SetUint64(4)
 	tx := core.InvokeTransaction{
 		Nonce:              nonce,
 		MaxFee:             &felt.Zero,
 		Version:            new(core.TransactionVersion).SetUint64(1),
 		ContractAddress:    contractAddress,
 		EntryPointSelector: utils.GetSelectorFromNameFelt(deployContractMethod),
+		CallData:           txCallData,
+		// CallData: []*felt.Felt{randata},
+	}
+
+	return &tx, nil
+
+}
+
+func NewDeployInvokeTestCoolOld() (*core.InvokeTransaction, error) {
+	//InvokeTx := rpc.InvokeTxnV1{
+	//	Version: rpc.TransactionV1,
+	//	Type:    rpc.TransactionType_Invoke,
+	//}
+
+	// Converting the contractAddress from hex to felt
+	// contractAddress := new(felt.Felt).SetUint64(2)
+	contractAddress, _ := new(felt.Felt).SetString("0x77fcc62a59a2160f099493fcd0466c526120320c164a62a72c6ac9931db34d9")
+
+	params := new(felt.Felt).SetUint64(1111)
+	// Building the functionCall struct, where :
+	FnCall := rpc.FunctionCall{
+		ContractAddress:    contractAddress,                               //contractAddress is the contract that we want to call
+		EntryPointSelector: utils.GetSelectorFromNameFelt(contractMethod), //this is the function that we want to call
+		Calldata:           []*felt.Felt{params},                          //this is the data that we want to pass to the function
+	}
+
+	txCallData := account.FmtCallDataCairo2([]rpc.FunctionCall{FnCall})
+
+	fmt.Println("invoke calldata = ", txCallData)
+
+	nonce := new(felt.Felt).SetUint64(5)
+	tx := core.InvokeTransaction{
+		Nonce:              nonce,
+		MaxFee:             &felt.Zero,
+		Version:            new(core.TransactionVersion).SetUint64(1),
+		ContractAddress:    contractAddress,
+		EntryPointSelector: utils.GetSelectorFromNameFelt(contractMethod),
 		CallData:           txCallData,
 		// CallData: []*felt.Felt{randata},
 	}
