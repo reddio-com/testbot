44591f823a5d989d5103821f5eddd7cb3d01532c
zhouqiang <zhouqiang.cl@gmail.com>
2024-02-02 09:37:09 +0800

    cool declare+deploy+invoke+call success (#22)
    * add deploy erc20 init
    
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- cairoVM/cmd/main.go
+++ cairoVM/cmd/main.go
@@ -27,8 +27,8 @@ func main() {
 
 	// declare TX
 	declareTx, class, err := cairoVM.NewDeclare(
-		"data/erc20.sierra.json",
-		"data/erc20.casm.json",
+		"data/cool.sierra.json",
+		"data/cool.casm.json",
 	)
 	if err != nil {
 		panic(err)
@@ -68,7 +68,7 @@ func main() {
 	fmt.Println("call response", utils.FeltToBigInt(resp[0]))
 
 	// deployContract TX
-	deployTx, err := cairoVM.NewDeployERC20()
+	deployTx, err := cairoVM.NewDeployCool()
 	if err != nil {
 		panic(err)
 	}
@@ -90,6 +90,40 @@ func main() {
 	}
 
 	new_address, _ := new(felt.Felt).SetString("0x7f2f788bcd85c25ece505a4fe359c577be77841c5afb971648af03391e5e834")
+
+	resp, err = vm.HandleCall(&rpc.FunctionCall{
+		ContractAddress:    *new_address,
+		EntryPointSelector: *utils.GetSelectorFromNameFelt("get_value"),
+	},
+		callClassHash,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("call response", utils.FeltToBigInt(resp[0]))
+
+	deployTx, err = cairoVM.NewDeployCoolOld()
+	if err != nil {
+		panic(err)
+	}
+
+	trace, err = vm.HandleInvokeTx(deployTx) // Assuming there is a HandleInvokeTx function
+	if err != nil {
+		panic(err)
+	}
+	spew.Dump(trace)
+
+	invokeTx, err = cairoVM.NewDeployInvokeTestCoolOld()
+	if err != nil {
+		panic(err)
+	}
+
+	trace, err = vm.HandleInvokeTx(invokeTx) // Assuming there is a HandleInvokeTx function
+	if err != nil {
+		panic(err)
+	}
+
+	new_address, _ = new(felt.Felt).SetString("0x77fcc62a59a2160f099493fcd0466c526120320c164a62a72c6ac9931db34d9")
 
 	resp, err = vm.HandleCall(&rpc.FunctionCall{
 		ContractAddress:    *new_address,
