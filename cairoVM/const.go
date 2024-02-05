3430aa268d434862b9a65c1eb730eccee595ef57
Qiang Zhou <zhouqiang.cl@gmail.com>
2024-02-05 10:09:30 +0800

    refine some function
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- cairoVM/const.go
+++ cairoVM/const.go
@@ -8,6 +8,7 @@ const (
 	NoValidateAccountClassHash = "0x24a22c925997bba54caa6ddf38b4d1616c9968e757d6846890b46caa88907e3"
 	UniversalDeployerClassHash = "0x4569ffd48c2a3d455437c16dc843801fb896b1af845bc8bc7ba83ebc4358b7f"
 	CoolContractClassHash      = "0x35eb1d3593b1fe9a8369a023ffa5d07d3b2050841cb75ad6ef00698d9307d10"
+	CoolOldConctractClassHash  = "0x47f93257c3a6e42fc71162a646b3223dfad27c2d994f97f333492c66e31b8c8"
 
 	RandomSalt = "0x53eb1d3593b1fe9a8369a023ffa5d07d3b2050841cb75ad6ef00698d9307d10"
 )
--- cairoVM/deploy.go
+++ cairoVM/deploy.go
@@ -12,14 +12,14 @@ var (
 	deployContractMethod string = "deployContract"
 )
 
-func NewDeployCool() (*core.InvokeTransaction, error) {
+func NewDeployCool(contractClassHash string) (*core.InvokeTransaction, error) {
 	contractAddress, err := new(felt.Felt).SetString(UniversalDeployerContractAddress)
 
 	if err != nil {
 		return nil, err
 	}
 
-	classHash, err := new(felt.Felt).SetString(CoolContractClassHash)
+	classHash, err := new(felt.Felt).SetString(contractClassHash)
 
 	if err != nil {
 		return nil, err
@@ -48,36 +48,6 @@ func NewDeployCool() (*core.InvokeTransaction, error) {
 		EntryPointSelector: utils.GetSelectorFromNameFelt(deployContractMethod),
 		CallData:           txCallData,
 	}
-	return &tx, nil
-
-}
-
-func NewDeployCoolOld() (*core.InvokeTransaction, error) {
-	contractAddress := new(felt.Felt).SetUint64(1)
-
-	classHash, _ := new(felt.Felt).SetString("0x47f93257c3a6e42fc71162a646b3223dfad27c2d994f97f333492c66e31b8c8")
-
-	salt, _ := new(felt.Felt).SetString("0x53eb1d3593b1fe9a8369a023ffa5d07d3b2050841cb75ad6ef00698d9307d10")
-
-	uniq := new(felt.Felt).SetUint64(1)
-
-	calldataLength := new(felt.Felt).SetUint64(0)
-
-	FnCall := rpc.FunctionCall{
-		ContractAddress:    contractAddress,                                     //contractAddress is the contract that we want to call
-		EntryPointSelector: utils.GetSelectorFromNameFelt(deployContractMethod), //this is the function that we want to call
-		Calldata:           []*felt.Felt{classHash, salt, uniq, calldataLength}, //this is the data that we want to pass to the function
-	}
-
-	txCallData := account.FmtCallDataCairo2([]rpc.FunctionCall{FnCall})
-
-	tx := core.InvokeTransaction{
-		Version:            new(core.TransactionVersion).SetUint64(1),
-		ContractAddress:    contractAddress,
-		EntryPointSelector: utils.GetSelectorFromNameFelt(deployContractMethod),
-		CallData:           txCallData,
-	}
-
 	return &tx, nil
 
 }
